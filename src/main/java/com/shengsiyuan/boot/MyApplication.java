37620bbf37bf4750e8f0db13310ce3bd4fffeeed
jbw <jb@98game.cn>
2020-01-13 21:06:59 +0800

    fix

--- src/main/java/com/shengsiyuan/boot/MyApplication.java
+++ src/main/java/com/shengsiyuan/boot/MyApplication.java
@@ -9,7 +9,7 @@ public class MyApplication {
 
     public static void main(String[] args) {
         //打印MyApplication的类加载器
-        //cdSystem.out.println("MyApplication classloader: " + MyApplication.class.getClassLoader());
+        System.out.println("MyApplication classloader: " + MyApplication.class.getClassLoader());
         SpringApplication.run(MyApplication.class, args);
     }
 }
